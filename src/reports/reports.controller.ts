3475e3f1af1138a263a6b9cb5e7b3b53c73b0592
amir <amir.developer8@gmail.com>
2023-01-06 03:41:57 +0330

    add some api for reports and add admin guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.intercept';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
+import { ApproveChangeDto } from './dtos/approve-change.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
@@ -16,5 +29,26 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveChange(@Param('id') id: string, @Body() body: ApproveChangeDto) {
+    return this.reportsService.changeApproval(parseInt(id), body.approved);
+  }
+
+  @Get('/:id')
+  async findOne(@Param('id') id: string) {
+    const report = await this.reportsService.findOne(parseInt(id));
+    if (!report) {
+      throw new NotFoundException('report not found!');
+    }
+
+    return report;
+  }
+
+  @Get()
+  getEstimates(@Query() query: GetEstimateDto) {
+    return this.reportsService.getEstimates(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,18 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/user.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
+  findOne(id: number) {
+    if (!id) {
+      return null;
+    }
+    const report = this.repo.findOne({ where: { id } });
+
+    return report;
+  }
+
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
     report.user = user;
 
     return this.repo.save(report);
+  }
+
+  async changeApproval(id: number, approved: boolean) {
+    const report = await this.findOne(id);
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+
+  getEstimates({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lat })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameter('mileage', mileage)
+      .limit(3)
+      .getRawOne();
   }
 }
